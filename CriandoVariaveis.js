aaa4cab796e9d8dff0772b63f70e9549a936445a
jocyneto <jocygneto@gmail.com>
2021-02-03 07:06:10 -0300

    publicando

---
+++ CriandoVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Criando/Manipulando variaveis");
+
+const numero = 10;
+
+console.log("Somando: "+(numero+1));
+console.log("Dividindo: "+(numero/2));
+console.log("Subtraindo: "+(numero-1));
+console.log("Multiplicando: "+(numero*5));
+
+const numeroSomado = numero + 10;
+
+console.log("Numero Somado com mais 10: "+numeroSomado);
+

7212b630f8042c1673bd8a0ac97bb501b78634b9
jocyneto <58772980+jocyneto@users.noreply.github.com>
2021-02-04 17:10:11 -0300

    t

--- Listas.js
+++ Listas.js
@@ -1,4 +1,4 @@
-console.log("Listas de Alunos:");
+console.log("--------------------------Listas de Alunos:");
 
 const listaDeAlunos = new Array(
     "joão"
@@ -13,3 +13,8 @@ console.log(listaDeAlunos);
 listaDeAlunos.splice(2,1); //irá remover apartir do index 2 e removerá apenas um elemneto
 console.log(listaDeAlunos);
 //Olhar o restante de metodos na documentação "Array JS"
+
+//para pegar uma posicao é igual no java
+console.log("Aluno: ", listaDeAlunos[3]);
+
+console.log();
